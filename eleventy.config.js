b0cfceda7562cd2c79f150cdde656b82616a8ff2
ma9y <184684405+ma9y@users.noreply.github.com>
2025-05-17 16:39:36 +0200

    initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,16 @@
 import 'dotenv/config'
+import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
-    
+
     eleventyConfig.addPassthroughCopy("views/assets/img");
     eleventyConfig.addPassthroughCopy("views/assets/js");
-    
+
+    // FILTERS
+    eleventyConfig.addFilter("postDate", (dateObj, format = "LLL d, yyyy") => {
+        return DateTime.fromJSDate(dateObj).toFormat(format);
+    });
+
 };
 
 export const config = {
