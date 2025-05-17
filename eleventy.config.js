44eb9265c6d18141c8fed678fe25d0c57e6c2d02
ma9y <184684405+ma9y@users.noreply.github.com>
2025-05-17 17:22:43 +0200

    initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,31 @@
 import 'dotenv/config'
 import { DateTime } from "luxon";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("views/assets/img");
     eleventyConfig.addPassthroughCopy("views/assets/js");
+
+    // PLUGINS
+    eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "post", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "cs",
+			title: "ma9y.net",
+			subtitle: "Deník",
+			base: "https://ma9y.net/",
+			author: {
+				name: "Fanda",
+				email: "", // Optional
+			}
+		}
+	});
 
     // FILTERS
     eleventyConfig.addFilter("postDate", (dateObj, format = "LLL d, yyyy") => {
